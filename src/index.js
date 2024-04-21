cacaba545cc7e737a45a94abb726d9a02dc06016
pablocrv12 <pablocarvajalbenitez@uma.es>
2024-04-21 18:45:32 +0200

    Se arregla cabecera Cors

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 const express = require("express");
+const cors = require('cors');
 const v1FlowRouter = require("./v1/routes/flowRoutes");
 const initDb = require("./config/db");
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
+
 app.use(express.json());
 app.use("/api/v1/flow", v1FlowRouter);
 
+
+
 // starting server
 app.listen(PORT,()=> {
-    console.log("servidor escuchando...");
+    console.log("servidor escuchando en el puerto: " +PORT);
 });
 
 initDb();
