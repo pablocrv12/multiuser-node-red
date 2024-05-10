b8f0087ce9f304e836515783f6ef62b618ba3f18
pablocrv12 <62345574+pablocrv12@users.noreply.github.com>
2024-05-11 00:53:43 +0200

    jwt users

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,46 +1,22 @@
-const { compareSync } = require('bcrypt');
-const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy;
+const JwtStrategy = require('passport-jwt').Strategy,
+ExtractJwt = require('passport-jwt').ExtractJwt;
+const opts = {}
 const UserModel = require('../models/User')
+const passport = require('passport')
 
-passport.use(new LocalStrategy(
-    function (email, password, done) {
-        console.log("Consultando la base de datos para el usuario con el correo electrónico:", email);
-        UserModel.findOne({ email: email }, function (err, user) {
-            if (err) { 
-                console.error("Error al consultar la base de datos:", err);
-                return done(err); 
-            }
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = 'Random string';
 
-            if (!user) {  
-                console.log("No se encontró ningún usuario con el correo electrónico:", email);
-                return done(null, false, { message: 'Incorrect email.' });
-            }
-
-            if (!compareSync(password, user.password)) { 
-                console.log("La contraseña proporcionada es incorrecta para el usuario:", email);
-                return done(null, false, { message: 'Incorrect password.' });
-            }
-
-            console.log("Inicio de sesión exitoso para el usuario:", email);
-            return done(null, user); 
-        });
-    }
-));
-
-//Persists user data inside session
-passport.serializeUser(function (user, done) {
-    done(null, user.id);
-});
-
-//Fetches session details using session id
-passport.deserializeUser(async function (id, done) {
-    try {
-        const user = await UserModel.findById(id);
-        console.log("Usuario deserializado:", user);
-        done(null, user);
-    } catch (error) {
-        console.error("Error al deserializar usuario:", error);
-        done(error);
-    }
-});
+passport.use(new JwtStrategy(opts, function (jwt_payload, done) {
+    UserModel.findOne({ id: jwt_payload.id }, function (err, user) {
+        if (err) {
+            return done(err, false);
+        }
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+            // or you could create a new account
+        }
+    });
+}));
--- src/index.js
+++ src/index.js
@@ -1,180 +1,110 @@
 const express = require("express");
 const cors = require('cors');
 const passport = require('passport');
-const session = require("express-session");
 const v1FlowRouter = require("./v1/routes/flowRoutes");
 const v1UserRouter = require("./v1/routes/userRoutes");
 const v1noderedRouter = require("./v1/routes/noderedRoutes");
 const initDb = require("./config/db");
 const path = require('path');
 const UserModel = require('./models/User')
-const MongoStore = require('connect-mongo')
-const { hashSync } = require('bcrypt');
-
+const { hashSync, compareSync } = require('bcrypt');
+const jwt = require('jsonwebtoken')
 
 
 const app = express();
-app.use(express.urlencoded({extended: true}));
 
-/*
-app.use(cookieParser('mi ultra hiper secreto'));
-app.use(session({
-secret: 'mi ultra hiper secreto',
-resave: true,
-saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use(new PassportLocal(function(email,password,done){
-    if(email === "codigofacilito" && password === "12345678")
-        return done(null, {id: 1, name: "Cody"});
-
-    done(null, false);  
-}))
-
-// serializacion
-passport.serializeUser(function(user,done){
-    done(null, user.id);
-});
-
-
-// deserializacion
-passport.deserializeUser(function(id, done,){
-    done(null, {id: 1, name: "Cody"})
-})
-
-*/
 
 const PORT = process.env.PORT || 3000;
 
-/*
-app.set('view engine', 'ejs');
-
-app.get("/", (req,res,next)=>{
-    // comprueba si ha iniciado sesion
-    if(req.isAuthenticated()) return next();
-
-    // si no ha iniciado sesion redirecciona a login
-    res.redirect("/login");
-}, (req,res) => {
-    // si ya iniciamos mostrar bienvenida
-
-    // Si no hemos iniciado sesión redireccionar a /login
-    res.send("hola");
-});
-
-app.get("/login", (req,res) => {
-    // Mostrar el formulario de login, hacerlo con react
-    res.render("login")
-});
-
-app.post("/login", passport.authenticate('local',{
-    successRedirect: "/", 
-    failureRedirect: "/login"
-    }));
-
-*/ 
-
-// tutorial
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
+app.use(express.json());
 app.use(express.urlencoded({extended: true}))
-app.use(session({
-    secret: 'mi ultra hiper secreto',
-    resave: false,
-    saveUninitialized: true,
-    store: MongoStore.create({mongoUrl: 'mongodb+srv://pablocarvajalbenitez:y1O44Y1Y9endY7Kb@clustertfg.bblxsmc.mongodb.net/Multiuser-Node-RED', collectionName: "sessions"}),
-    cookie: {
-        maxAge : 1000 * 60 * 60 * 24
-    }
-    }));
+app.use(cors());
+app.use(passport.initialize())
 
 require('./config/passport')
-
-app.use(passport.initialize())
-app.use(passport.session())
 
 app.get('/login', (req,res) => {
     res.render('login')
 })  
 
-app.get('/register', (req,res) => {
+app.get('/register', (req, res) => {
     res.render('register')
-})  
+})
 
-    app.post('/login', (req, res, next) => {
-        console.log("Datos recibidos en la solicitud POST /login:", req.body); // Agregar este console.log para ver los datos recibidos en la solicitud POST
+app.post('/login', (req, res) => {
+    UserModel.findOne({ email: req.body.email }).then(user => {
+        //No user found
+        if (!user) {
+            return res.status(401).send({
+                success: false,
+                message: "Could not find the user."
+            })
+        }
 
-        passport.authenticate('local', (err, user, info) => {
-            if (err) {
-                console.error("Error durante la autenticación:", err); // Agregar este console.error para mostrar errores durante la autenticación
-                return next(err);
-            }
-            if (!user) {
-            
-                console.log("Usuario no encontrado o contraseña incorrecta:", info.message); // Agregar este console.log para mostrar el mensaje de error de autenticación
-                return res.redirect('/login?error=' + encodeURIComponent(info.message));
-            }
-            req.login(user, (err) => {
-                if (err) {
-                    console.error("Error al iniciar sesión:", err); // Agregar este console.error para mostrar errores al iniciar sesión
-                    return next(err);
-                }
-                console.log("Inicio de sesión exitoso para el usuario:", user); // Agregar este console.log para mostrar información sobre el usuario que inició sesión con éxito
-                return res.redirect('/protected');
-            });
-        })(req, res, next);
-    });
+        //Incorrect password
+        if (!compareSync(req.body.password, user.password)) {
+            return res.status(401).send({
+                success: false,
+                message: "Incorrect password"
+            })
+        }
+
+        const payload = {
+            email: user.email,
+            id: user._id
+        }
+
+        const token = jwt.sign(payload, "Random string", { expiresIn: "1d" })
+
+        return res.status(200).send({
+            success: true,
+            message: "Logged in successfully!",
+            token: "Bearer " + token
+        })
+    })
+})
 
 app.post('/register', (req, res) => {
-    let user = new UserModel({
+    const user = new UserModel({
         email: req.body.email,
-        password: hashSync(req.body.password, 10)  
-    });
-
-    user.save()
-        .then(user => {
-            // Iniciar sesión después de que el usuario se haya guardado exitosamente
-            req.login(user, err => {
-                if (err) {
-                    return next(err);
-                }
-                // Redireccionar o enviar la respuesta según sea necesario
-                res.redirect('/protected');
-            });
+        password: hashSync(req.body.password, 10)
+    })
+    
+    user.save().then(user => {
+        res.send({
+            success: true,
+            message: "User created successfully.",
+            user: {
+                id: user._id,
+                email: user.email
+            }
         })
-        .catch(err => {
-            // Manejo de errores en caso de que falle el registro del usuario
-            console.error(err);
-            res.status(500).send({ success: false, error: 'Error en el registro de usuario' });
-        });
-});
+    }).catch(err => {
+        res.send({
+            success: false,
+            message: "Something went wrong",
+            error: err
+        })
+    })
+})
 
 app.get('/logout', (req, res) => {
-    req.logout((err) => {
-        if (err) {
-            console.error('Error al cerrar sesión:', err);
-            return next(err);
-        }
-        res.redirect("/login");
-    });
+
 });
 
-app.get('/protected', (req, res) => {
-    if (req.isAuthenticated()) {
-        res.send("Protected")
-    } else {
-        res.status(401).send({ msg: "Unauthorized" })
-    }
-    console.log(req.session)
-    console.log(req.user)
-}) 
+app.get('/protected', passport.authenticate('jwt', { session: false }), (req, res) => {
+    return res.status(200).send({
+        success: true,
+        user: {
+            id: req.user._id,
+            email: req.user.email,
+        }
+    })
+})
 
-app.use(cors());
 
-app.use(express.json());
 app.use("/api/v1/flow", v1FlowRouter);
 app.use("/api/v1/user", v1UserRouter);
 app.use("/api/v1/node", v1noderedRouter);
