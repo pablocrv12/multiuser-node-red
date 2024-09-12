e4c1093107d893ca0061b3a9415f23182abbf3a1
pablocrv12 <62345574+pablocrv12@users.noreply.github.com>
2024-09-12 19:31:35 +0200

    Fixed port bug in Google Cloud

--- src/index.js
+++ src/index.js
@@ -4,19 +4,26 @@ const passport = require('passport');
 const v1FlowRouter = require("./v1/routes/flowRoutes");
 const v1ClassRouter = require("./v1/routes/classRoutes");
 const v1UserRouter = require("./v1/routes/userRoutes");
-const v1noderedRouter = require("./v1/routes/nodeRedRoutes");
+const v1noderedRouter = require("./v1/routes/noderedRoutes");
 const v1passwordResetRouter = require("./v1/routes/passwordResetRoutes");
 const v1EmailRouter = require("./v1/routes/emailRoutes");
 const initDb = require("./config/db");
 const path = require('path'); 
+const UserModel = require('./models/User');
+const { hashSync, compareSync } = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { exec } = require('child_process');
 require('dotenv').config();
-
-
-
+const util = require('util');
+const execPromise = util.promisify(exec);
 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+let portCounter = 1880; // Puerto inicial
+const userPortMap = {}; // Mapeo de userId a puertos
+
 
 
 app.use(express.json());
@@ -25,6 +32,24 @@ app.use(cors());
 app.use(passport.initialize());
 
 require('./config/passport');
+
+
+// Ruta al archivo de la cuenta de servicio
+const keyFilePath = path.join(__dirname, 'multiuser-node-red-a7bc8e2d8abd.json');
+        
+
+// Función para ejecutar comandos
+function execCommand(command) {
+    return new Promise((resolve, reject) => {
+        exec(command, (error, stdout, stderr) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(stdout.trim());
+            }
+        });
+    });
+}
 
 
 // Rutas API
